2739bd32be3705cf4cd1539303f6de88c0fd1c33
Chris <chris.sedano.arenas@gmail.com>
2020-08-07 10:34:32 +0100

    Adding The Ship, Up And Down
    In PlanetViewVC.swift, implement new func addShip with some constants and moviment for ship

--- Controller/PlanetViewVC.swift
+++ Controller/PlanetViewVC.swift
@@ -25,12 +25,11 @@ class PlanetViewVC: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         addBaseNode()
         addPlanet()
+        addText()
+        addShip()
         
         let gesture = UISwipeGestureRecognizer(target: self, action: #selector(dismiss(fromGesture:)))
         sceneView.addGestureRecognizer(gesture)
-        
-        // let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -66,6 +65,66 @@ class PlanetViewVC: UIViewController, ARSCNViewDelegate {
         let planetRotate = SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 25)
         let repeatRotate = SCNAction.repeatForever(planetRotate)
         planetNote.runAction(repeatRotate)
+    }
+    
+    func addText() {
+        
+        let text = SCNText(string: planetName.capitalized, extrusionDepth: 1)
+        text.font = UIFont(name: "futura", size: 16)
+        text.flatness = 0
+        
+        let scaleFactor = 0.1 / text.font.pointSize
+        textNode.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        textNode.geometry = text
+        
+        let max = textNode.boundingBox.max.x
+        let min = textNode.boundingBox.min.x
+        let midpoint = -( (max - min) / 2 + min) * Float(scaleFactor)
+        
+        textNode.position = SCNVector3(midpoint, 0.35, 0)
+        baseNote.addChildNode(textNode)
+    }
+    
+    func addShip() {
+        
+        let orbitAction = SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 6)
+        let repeatOrbit = SCNAction.repeatForever(orbitAction)
+        
+        let wait = SCNAction.wait(duration: 1)
+        
+        let shipUpAction = SCNAction.move(to: SCNVector3(-0.35, 0.2, 0), duration: 2)
+        shipUpAction.timingMode = .easeInEaseOut
+        
+        let shipDownAction = SCNAction.move(to: SCNVector3(-0.35, -0.2, 0), duration: 2)
+        shipDownAction.timingMode = .easeInEaseOut
+        
+        let upDown = SCNAction.sequence([shipUpAction, wait, shipDownAction])
+        let repeatUpDown = SCNAction.repeatForever(upDown)
+        
+        let pitchUpAction = SCNAction.rotateTo(x: -0.3, y: 0.0, z: -0.17, duration: 1)
+        pitchUpAction.timingMode = .easeInEaseOut
+        let levelOff = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: 1)
+        let shipReversePitch = SCNAction.rotateTo(x: 0.3, y: 0.0, z: 0.17, duration: 1)
+        shipReversePitch.timingMode = .easeInEaseOut
+        let pitchUpDown = SCNAction.sequence([pitchUpAction, levelOff, wait, shipReversePitch, levelOff, wait])
+        let repeatPitch = SCNAction.repeatForever(pitchUpDown)
+        
+        
+        let scene = SCNScene(named: "art.scnassets/ship.scn")
+            
+        if let shipNode = scene?.rootNode.childNode(withName: "ship", recursively: true) {
+            shipNode.scale = SCNVector3(0.2, 0.2, 0.2)
+            shipNode.position = SCNVector3(-0.35, 0, 0)
+            
+            let rotateNode = SCNNode()
+            baseNote.addChildNode(rotateNode)
+            rotateNode.addChildNode(shipNode)
+            rotateNode.runAction(repeatOrbit)
+            
+            rotateNode.runAction(repeatPitch)
+            
+            shipNode.runAction(repeatUpDown)
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
